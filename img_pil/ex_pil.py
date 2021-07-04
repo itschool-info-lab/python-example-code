cc9beabcad872bb6d8f0dc29f0ce243f04c31c5c
info-lab <min.sik@kakaocorp.com>
2021-07-04 18:16:17 +0900

    Example Update

--- img_pil/ex_pil.py
+++ img_pil/ex_pil.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from PIL import Image
+from PIL import ImageFilter
 
 # 1. 이미지 파일 기본 정보 읽기 #
 try:
@@ -25,6 +26,96 @@ try:
     print("이미지 가로:", img_width)
     print("이미지 세로:", img_height)
     print("이미지 모드:", change_im.mode)
+except OSError as e:
+    print(e)
+
+
+# 3. 이미지 썸네일 만들기 #
+try:
+    im = Image.open("anchors.png")
+    img_width, img_height = im.size
+    print("이미지 사이즈:", im.size)
+    print("이미지 가로:", img_width)
+    print("이미지 세로:", img_height)
+    print("이미지 모드:", im.mode)
+
+    size = (100, 100)
+    im.thumbnail(size)
+    im.save("anchors_thumbnail.png")
+    thum_im = Image.open("anchors_thumbnail.png")
+    img_width, img_height = thum_im.size
+    print("썸네일 이미지 사이즈:", thum_im.size)
+    print("썸네일 이미지 가로:", img_width)
+    print("썸네일 이미지 세로:", img_height)
+    print("썸네일 이미지 모드:", thum_im.mode)
+except OSError as e:
+    print(e)
+
+
+# 4. 이미지 크기 변경 #
+try:
+    im = Image.open("anchors.png")
+    img_width, img_height = im.size
+    print("이미지 사이즈:", im.size)
+    print("이미지 가로:", img_width)
+    print("이미지 세로:", img_height)
+    print("이미지 모드:", im.mode)
+
+    size = (1000, 1000)
+    resize_img = im.resize(size)
+    resize_img.save('anchors_resize.png')
+
+    resize_im = Image.open("anchors_resize.png")
+    img_width, img_height = resize_im.size
+    print("크기변경 이미지 사이즈:", resize_im.size)
+    print("크기변경 이미지 가로:", img_width)
+    print("크기변경 이미지 세로:", img_height)
+    print("크기변경 이미지 모드:", resize_im.mode)
+except OSError as e:
+    print(e)
+
+
+# 5. 이미지 회전 각도 변경 #
+try:
+    im = Image.open("anchors.png")
+    img_width, img_height = im.size
+    print("이미지 사이즈:", im.size)
+    print("이미지 가로:", img_width)
+    print("이미지 세로:", img_height)
+    print("이미지 모드:", im.mode)
+
+    rotate = 90
+    rotate_img = im.rotate(rotate)
+    rotate_img.save('anchors_rotate.png')
+
+    rotate_im = Image.open("anchors_rotate.png")
+    img_width, img_height = rotate_im.size
+    print("회전 각도 이미지 사이즈:", rotate_im.size)
+    print("회전 각도  이미지 가로:", img_width)
+    print("회전 각도  이미지 세로:", img_height)
+    print("회전 각도  이미지 모드:", rotate_im.mode)
+except OSError as e:
+    print(e)
+
+
+# 6. 이미지 필터 적용 #
+try:
+    im = Image.open("anchors.png")
+    img_width, img_height = im.size
+    print("이미지 사이즈:", im.size)
+    print("이미지 가로:", img_width)
+    print("이미지 세로:", img_height)
+    print("이미지 모드:", im.mode)
+
+    filter_img = im.filter(ImageFilter.BLUR)
+    filter_img.save("anchors_filter.png")
+
+    filter_im = Image.open("anchors_filter.png")
+    img_width, img_height = filter_im.size
+    print("필터 이미지 사이즈:", filter_im.size)
+    print("핕터 이미지 가로:", img_width)
+    print("핕터 이미지 세로:", img_height)
+    print("핕터 이미지 모드:", filter_im.mode)
 except OSError as e:
     print(e)
 
